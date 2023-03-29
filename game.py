69abe92c51a7c52e286086f4e76710d87dcd11c5
ThanosArapakis1997 <arapakis_thanos@hotmail.com>
2023-03-30 00:05:22 +0300

    A first AI touch

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import gym
 import pygame
 import random
 from block import Block
@@ -5,10 +6,16 @@ from head import Head
 from body import Body
 from food import Food
 from snake import Snake
+import gym
+from gym import envs
+from keras.models import Sequential
+from keras.layers import Dense,Flatten
+from keras.optimizers import Adam
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
 
-
-
-WINDOW_SIZE      = (500, 500)
+WINDOW_SIZE      = (200, 200)
 SIZE             = 50
 
 class Game():
@@ -20,9 +27,22 @@ class Game():
         self.snake       = Snake(Head(WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2))
         self.food        = Food(0, 0) 
         self.win_flag    = False
+        self.AiTraining  = True
         self.history     = []
 
 
+    def _model(states,actions):
+        model= Sequential()
+        model.add(Flatten(input_shape=(1,states)))
+        model.add(Dense(24,activation="relu"))
+        model.add(Dense(actions,activation="linear"))
+        return model
+
+    def _build_agent(model, actions):
+        policy= BoltzmannQPolicy()
+        memory= SequentialMemory(limit=50000, window_length=1)
+        dqn= DQNAgent(model,mempry=memory, policy=policy,nb_actions=actions,nb_steps_warmup=10,target_model_update=1e-2)
+        return dqn
 
     def _new_food(self):
         game_space = []
@@ -53,8 +73,10 @@ class Game():
 
     def main(self):
         running = True
+        #env= gym.make()
         print("Score: ", self.score)
-        while running:
+        if(self.AiTraining == False):
+         while running:
             state = self._game_state()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
@@ -133,6 +155,30 @@ class Game():
             self.food._draw(self.display)
 
             pygame.display.flip()
+        else:
+            env=gym.make('SpaceInvaders-v0')
+            height, width, channels= env.observation_space.shape
+            actions= env.action_space.shape
+            actions= [[1,0,0],[0,1,0],[0,0,1]]
+            states=  [[1.0, 0, 0, 0],[0, 1.0, 0, 0],[0, 0, 1.0, 0],[0, 0, 0, 1.0]]
+            episodes= 10
+            for episode in episodes:
+                state= env.reset()
+                done= False
+                score=0
+                while not done:
+                    env.render()
+                    action= random.choice([0,1,2,3,4,5])
+                    n_state,reward,done,info= env.step(action)
+                    score+=reward
+                print('Episode{} Score{}'.format(episode,score))
+            env.close()
+            model= self._model(len(states), len(actions))
+            dqn= self._build_agent(model,len(actions))
+            dqn.compile(Adam(lr=1e-3),metrics=['mae'])
+            dqn.fit()
+
+
 
 
 game = Game()
